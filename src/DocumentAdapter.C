a1200d3fb5a302c66a0f68f8def81a16acc63c0b
Stuart <swheaton5@gmail.com>
2017-10-16 23:17:43 +0000

    finished Document and Node Adapters. Some functionality not there, waiting on other node type adapters

--- src/DocumentAdapter.C
+++ src/DocumentAdapter.C
@@ -1,9 +1,1 @@
 #include "DocumentAdapter.H"
-
-XERCES::DOMElement     *dom::DocumentAdapter::createElement(const XERCES::XMLCh *tagName)
-{
-    // Returns Element_Impl which is a dom::Element
-    dom::Node * retElt = adaptee->createElement(std::string(tagName));
-    //dom::Node * retNode = retElt;
-    //return new NodeAdapter(retNode);
-}
--- src/NodeAdapter.C
+++ src/NodeAdapter.C
@@ -1,6 +1,1 @@
 #include "NodeAdapter.H"
-
-XERCES::DOMNodeList    *dom::NodeAdapter::getChildNodes() const
-{
-    //return dynamic_cast<XERCES::DOMNodeList*>(adaptee->getChildNodes());
-}
